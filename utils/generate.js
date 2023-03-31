fedca1305b8eecd914f2c0fa27d7ba552d8a426d
msaaddev <mrsaadirfan@gmail.com>
2023-03-31 16:19:04 +0200

    📦 NEW: Added AI + Rapid template generation

--- utils/generate.js
+++ utils/generate.js
@@ -5,6 +5,7 @@ const { green: g, cyan: c, yellow: y, dim: d } = require('chalk');
 const handleErr = require('cli-handle-error');
 const copy = require('copy-template-dir');
 const path = require('path');
+const fs = require('fs/promises');
 
 const spinner = ora({ text: '' });
 
@@ -12,9 +13,9 @@ module.exports = (dirName, vars) => {
 	const outDir = vars.name;
 	const type = vars.type;
 	const outDirPath = path.join(process.cwd(), outDir);
-	const inDir = path.join(dirName, `template/${type}`);
 
-	if (type === `rest`) {
+	if (type === `REST`) {
+		const inDir = path.join(dirName, `templates/${type.toLowerCase()}`);
 		copy(inDir, outDirPath, vars, async (err, files) => {
 			if (err) handleErr(err);
 
@@ -55,7 +56,8 @@ module.exports = (dirName, vars) => {
 		});
 	}
 
-	if (type === `graphql`) {
+	if (type === `GraphQL`) {
+		const inDir = path.join(dirName, `templates/${type.toLowerCase()}`);
 		copy(inDir, outDirPath, vars, async (err, files) => {
 			if (err) handleErr(err);
 
@@ -92,6 +94,55 @@ module.exports = (dirName, vars) => {
 			console.log();
 			console.log(`${c(`cd`)} ${outDir}`);
 			console.log(`${c(`npm run server`)}`);
+			console.log();
+		});
+	}
+
+	if (type === `Rapid API + Next.js + OpenAI`) {
+		const inDir = path.join(dirName, `templates/rapid-openai`);
+		copy(inDir, outDirPath, vars, async (err, files) => {
+			if (err) handleErr(err);
+
+			const env = `OPENAI_API_KEY=${vars.apiKeysToReturn.openAIKey}
+RAPIDAPI_KEY=${vars.apiKeysToReturn.rapidAPIKey}
+RAPIDAPI_URL=`;
+
+			await fs.writeFile(path.join(outDirPath, `.env.local`), env);
+
+			console.log();
+			console.log(
+				`${d(`Creating files in ${g(`./${outDir}`)} directory:`)}`
+			);
+			console.log();
+
+			files.forEach(file => {
+				const fileName = path.basename(file);
+				console.log(`${g(`CREATED`)} ${fileName}`);
+			});
+			console.log(`${g(`CREATED`)} .env.local`);
+			console.log();
+
+			spinner.start(
+				`${y(`DEPENDENCIES`)} installing…\n\n${d(
+					`This may take a while...`
+				)}`
+			);
+			process.chdir(outDirPath);
+			await command(`npm install express express-graphql graphql`);
+			await command(`npm install -D nodemon`);
+			await command(`npm dedupe`);
+			spinner.succeed(`${g(`DEPENDENCIES`)} installed!`);
+
+			alert({
+				type: 'success',
+				name: 'Rapid API + Next.js + OpenAI app created',
+				msg: `\n\n${files.length} files created in ./${outDir} directory.`
+			});
+
+			console.log(`${d(`I suggest that you begin by typing:`)}`);
+			console.log();
+			console.log(`${c(`cd`)} ${outDir}`);
+			console.log(`${c(`npm run dev`)}`);
 			console.log();
 		});
 	}
